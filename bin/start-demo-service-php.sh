4850ade15fe0b0465ae4d0b4d7b51f6e8c5220ac
Bruno Oliveira da Silva <bruno@abstractj.org>
2018-08-15 00:09:24 -0300

    Added the future generic adapter to Deployment config Still very WIP

---
+++ bin/start-demo-service-php.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+eval $(tmp/minishift oc-env)
+
+KEYCLOAK_URL=https://`oc get routes keycloak -o jsonpath='{.spec.host}'`/auth
+
+oc get buildconfigs demo-service-php &>/dev/null || oc new-build --binary=true --name=demo-service-php
+oc start-build demo-service-php --from-dir=demo-service-php --follow
+
+oc new-app -f demo-service-php/demo-service-php.json -p KEYCLOAK_URL=$KEYCLOAK_URL
